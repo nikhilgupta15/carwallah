1e245c7dc1bd328538b1a54dc40ede325b4c9654
Nikhil <nikhilgupta150498@gmail.com>
2023-09-13 19:35:03 +0530

    Added web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Footer, Navbar } from "@/components";
 import "./globals.css";
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "CarWallah",
@@ -18,6 +19,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
